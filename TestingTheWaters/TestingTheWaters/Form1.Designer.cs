51c040e0ae4642a9605ca70b47381253a3580b0f
Ben <eldutche@gmail.com>
2019-07-17 20:35:36 -0400

    This is the develop branch, brought to you by Jabba's palace

--- TestingTheWaters/TestingTheWaters/Form1.Designer.cs
+++ TestingTheWaters/TestingTheWaters/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.change = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(13, 62);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(75, 23);
+            this.change.TabIndex = 0;
+            this.change.Text = "Jabba";
+            this.change.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.change);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button change;
     }
 }
 
