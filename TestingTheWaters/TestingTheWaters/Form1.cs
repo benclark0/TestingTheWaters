a8b589493ea7c1f8633a0b481706ba60ccd55f65
Ben <eldutche@gmail.com>
2019-07-17 20:15:57 -0400

    Initial Code Base for Testing
    This is a description of what i'm trying to do!
    This is a test on the develop branch
    
    This is a test on the develop branch

---
+++ TestingTheWaters/TestingTheWaters/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingTheWaters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(@"These are not the droids you are looking for");
+        }
+    }
+}
